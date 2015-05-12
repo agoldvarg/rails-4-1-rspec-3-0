353c633a10db9ee67080a8f52a8f49787319cb2c
agoldvarg <agoldvarg@gmail.com>
2015-05-12 15:37:00 -0400

    dry out by_letter spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -5,8 +5,8 @@ describe Contact do
     contact = Contact.new(
       firstname: 'Aaron',
       lastname: 'Sumner',
-      email: 'tester@example.com'
-      )
+      email: 'tester@example.com')
+
     expect(contact).to be_valid
   end
 
@@ -45,51 +45,40 @@ describe Contact do
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
-  describe 'instance methods' do
-    describe '#name' do
-      it "returns a contact's full name as a string" do
-        contact = Contact.new(firstname: 'John', lastname: 'Doe',
-          email: 'johndoe@example.com')
+  describe '#name' do
+    it "returns a contact's full name as a string" do
+      contact = Contact.new(firstname: 'John', lastname: 'Doe',
+        email: 'johndoe@example.com')
 
-        expect(contact.name).to eq('John Doe')
-      end
+      expect(contact.name).to eq('John Doe')
     end
   end
 
-  describe 'class methods' do
-    describe '::by_letter' do
+  describe '::by_letter' do
+    before :each do
+      @smith = Contact.create(
+        firstname: 'John',
+        lastname: 'Smith',
+        email: 'jsmith@example.com')
+      @jones = Contact.create(
+        firstname: 'Tim',
+        lastname: 'Jones',
+        email: 'tjones@example.com')
+      @johnson = Contact.create(
+        firstname: 'John',
+        lastname: 'Johnson',
+        email: 'jjohnson@example.com')
+    end
+
+    context 'matching letters' do
       it 'returns a sorted array of results that match' do
-        smith = Contact.create(
-          firstname: 'John',
-          lastname: 'Smith',
-          email: 'jsmith@example.com')
-        jones = Contact.create(
-          firstname: 'Tim',
-          lastname: 'Jones',
-          email: 'tjones@example.com')
-        johnson = Contact.create(
-          firstname: 'John',
-          lastname: 'Johnson',
-          email: 'jjohnson@example.com')
-
-        expect(Contact.by_letter("J")).to eq([johnson, jones])
+        expect(Contact.by_letter("J")).to eq([@johnson, @jones])
       end
+    end
 
+    context 'non-matching letters' do
       it 'omits results that do not match' do
-        smith = Contact.create(
-          firstname: 'John',
-          lastname: 'Smith',
-          email: 'jsmith@example.com')
-        jones = Contact.create(
-          firstname: 'Tim',
-          lastname: 'Jones',
-          email: 'tjones@example.com')
-        johnson = Contact.create(
-          firstname: 'John',
-          lastname: 'Johnson',
-          email: 'jjohnson@example.com')
-
-        expect(Contact.by_letter("J")).not_to include(smith)
+        expect(Contact.by_letter("J")).not_to include(@smith)
       end
     end
   end
