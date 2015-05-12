6e37b8798eb9066dbb65f3828bdf8ab8cfd88639
agoldvarg <agoldvarg@gmail.com>
2015-05-12 16:46:56 -0400

    add Faker to contact Factory

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :contact do
-    firstname "John"
-    lastname "Doe"
-    sequence(:email) { |i| "johndoe#{i}@example"}
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    email { Faker::Internet.email }
   end
 end
