c9e91b505890b13fc6cfd4c1ba4d3f8522316441
agoldvarg <agoldvarg@gmail.com>
2015-05-12 15:04:26 -0400

    write contact model specs

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,10 +1,53 @@
 require 'rails_helper'
 
 describe Contact do
-  it "is valid with a firstname, lastname, and email"
-  it "is invalid without a firstname"
-  it "is invalid without a lastname"
-  it "is invalid without an email address"
-  it "is invalid with a dubplicate email address"
-  it "returns a contact's full name as a string"
+  it "is valid with a firstname, lastname, and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com'
+      )
+    expect(contact).to be_valid
+  end
+
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+
+  it "is invalid without a lastname" do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+
+  it "is invalid without an email address" do
+    contact = Contact.new(email: nil)
+    contact.valid?
+    expect(contact.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with a dubplicate email address" do
+    Contact.create(
+      firstname: 'Joe', lastname: 'Tester',
+      email: 'tester@example'
+      )
+    contact = Contact.new(
+      firstname: 'Jane', lastname: 'Tester',
+      email: 'tester@example'
+      )
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
+  end
+
+  describe 'instance methods' do
+    describe '#name' do
+      it "returns a contact's full name as a string" do
+        contact = Contact.new(firstname: 'John', lastname: 'Doe',
+          email: 'johndoe@example.com')
+        expect(contact.name).to eq('John Doe')
+      end
+    end
+  end
 end
