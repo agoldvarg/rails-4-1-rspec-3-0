af137c16ce41863a353f9fab6c1c0d4ffb3d4a10
agoldvarg <agoldvarg@gmail.com>
2015-05-12 16:42:43 -0400

    add phone sub factories and refactor phone spec

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,34 +2,21 @@ require 'rails_helper'
 
 describe Phone do
   it 'does not allow duplicate phone numbers per contact' do
-    contact = Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example.com')
-    contact.phones.create(
-      phone_type: 'home',
+    contact = create(:contact)
+    create(:home_phone,
+      contact: contact,
       phone: '555-555-5555')
-    mobile_phone = contact.phones.build(
-      phone_type: 'mobile',
+    mobile_phone = build(:mobile_phone,
+      contact: contact,
       phone: '555-555-5555')
-    mobile_phone.valid?
 
+    mobile_phone.valid?
     expect(mobile_phone.errors[:phone]).to include('has already been taken')
   end
 
   it 'allows two contacts to share a phone number' do
-    contact = Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'joetester@example')
-    contact.phones.create(
-      phone_type: 'home',
-      phone: '555-555-5555')
-    other_contact = Contact.new
-    other_phone = other_contact.phones.build(
-      phone_type: 'home',
-      phone: '555-555-5555')
+    create(:home_phone, phone: '555-555-5555')
 
-    expect(other_phone).to be_valid
+    expect(build(:mobile_phone, phone: '555-555-5555')).to be_valid
   end
 end
