a328b650368a8362d4c628e65744794707685d4b
agoldvarg <agoldvarg@gmail.com>
2015-05-13 13:56:04 -0400

    add specs for get requests on contacts controller

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -3,29 +3,75 @@ require 'rails_helper'
 describe ContactsController do
   describe 'GET #index' do
     context 'with params[:letter]' do
-      it "populates an array of contacts starting with the letter"
-      it "renders the :index template"
+      it "populates an array of contacts starting with the letter" do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index, letter: 'S'
+        expect(assigns(:contacts)).to match_array([smith])
+      end
+
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
     end
 
     context 'without params[:letter]' do
-      it "populates an array of all contacts"
-      it "renders the :index template"
+      it "populates an array of all contacts" do
+        smith = create(:contact, lastname: 'Smith')
+        jones = create(:contact, lastname: 'Jones')
+        get :index
+        expect(assigns(:contacts)).to match_array([smith, jones])
+      end
+      it "renders the :index template" do
+        get :index
+        expect(response).to render_template :index
+      end
     end
   end
 
   describe 'GET #show' do
-    it "assigns the requested contact to @contact"
-    it "renders the :show template"
+    before :each do
+      @contact = create(:contact)
+      get :show, id: @contact
+    end
+
+    it "assigns the requested contact to @contact" do
+      expect(assigns(:contact)).to eq @contact
+    end
+
+    it "renders the :show template" do
+      expect(response).to render_template :show
+    end
   end
 
   describe 'GET #new' do
-    it "assigns a new Contact to @contact"
-    it "renders the :new template"
+    before :each do
+      get :new
+    end
+
+    it "assigns a new Contact to @contact" do
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+
+    it "renders the :new template" do
+      expect(response).to render_template :new
+    end
   end
 
   describe 'GET #edit' do
-    it "assigns the requested contact to @contact"
-    it "renders the :edit template"
+    before :each do
+      @contact = create(:contact)
+      get :edit, id: @contact
+    end
+
+    it "assigns the requested contact to @contact" do
+      expect(assigns(:contact)).to eq @contact
+    end
+
+    it "renders the :edit template" do
+      expect(response).to render_template :edit
+    end
   end
 
   describe 'POST #create' do
