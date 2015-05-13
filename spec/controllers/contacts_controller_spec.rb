5dfa1256bcc3a314814cbc6d04789e488a432d46
agoldvarg <agoldvarg@gmail.com>
2015-05-13 14:47:34 -0400

    add specs for delete

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -160,7 +160,19 @@ describe ContactsController do
   end
 
   describe 'DELETE #destroy' do
-    it "deletes the contact from the database"
-    it "redirects to users#index"
+    before :each do
+      @contact = create(:contact)
+    end
+
+    it "deletes the contact from the database" do
+      expect{
+        delete :destroy, id: @contact
+      }.to change(Contact, :count).by(-1)
+    end
+
+    it "redirects to users#index" do
+      delete :destroy, id: @contact
+      expect(response).to redirect_to contacts_url
+    end
   end
 end
